3f2ee9440b7b8040ce651250b7af9752fc79eec3
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2026-06-04 23:03:09 +0300

    Configured ScalarAPI

--- Program.cs
+++ Program.cs
@@ -1,22 +1,40 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SwiftParser API",
+        Version = "v1",
+        Description = "API for parsing SWIFT messages and extracting relevant information",
+    });
+});
 
-WebApplication app = builder.Build();
-
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapSwagger("/swagger/{documentName}/swagger.json");
+
+    app.MapScalarApiReference("/", options =>
+    {
+        options.WithTitle("SwiftParser API Reference")
+          .ForceDarkMode()
+          .HideSearch()
+          .ShowOperationId()
+          .ExpandAllTags()
+          .SortTagsAlphabetically()
+          .SortOperationsByMethod()
+          .PreserveSchemaPropertyOrder()
+          .DisableAgent()
+          .WithOpenApiRoutePattern("/swagger/{documentName}/swagger.json");
+    });
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
